f1213643698bb654f6839ded0fd424b87abab96f
Katlyn W. <krw8ateez@gmail.com>
2024-09-27 11:45:55 -0400

    Add project files.

---
+++ Ch4Ex10_Katlyn_Winkelhake.cpp
@@ -0,0 +1,34 @@
+/*
+File Name: Ch4Ex10_Katlyn_Winkelhake.cpp
+Date: 9/27/2024
+Programmer: Katlyn Winkelhake
+Requirements:
+Write a program that asks the user to enter the month (letting the user 
+enter an integer in the range of 1 through 12) and the year.
+The program should then display the number of days in that month.
+Determine whether the year is divisible by 100. If it is, then it is a leap year if
+and only if it is divisible by 400. For example, 2000 is a leap year but 2100 is not.
+If the year is not divisible by 100, then it is a leap year if and only if it is divisible by 4.
+For example, 2008 is a leap year but 2009 is not.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
